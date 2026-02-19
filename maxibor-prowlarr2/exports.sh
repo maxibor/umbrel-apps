2b90b976a8dc640f73f254176b4463852c9071fa
maxibor <maxime.borry@tron-mainz.de>
2026-02-19 15:08:17 +0100

    cleanup: remove prowlarr test

--- maxibor-prowlarr2/exports.sh
+++
@@ -1,4 +0,0 @@
-export APP_PROWLARR_RADARR_CONFIG_XML=$(cat "${UMBREL_ROOT}/app-data/radarr/data/config/config.xml" 2>/dev/null || echo "")
-export APP_PROWLARR_SONARR_CONFIG_XML=$(cat "${UMBREL_ROOT}/app-data/sonarr/data/config/config.xml" 2>/dev/null || echo "")
-
-export APP_PROWLARR_QBITTORRENT_INSTALLED="true"
